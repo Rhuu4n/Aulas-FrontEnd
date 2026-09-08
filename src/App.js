3b5e267d07c6ece7cc86314ec571b3c43654b977
RHUAN DE PADUA OLIVEIRA <rhuan.padua@senaisp.edu.br>
2026-09-08 20:46:43 -0300

    feat: Exercicio Aviso com temporizador completo e funcional

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import Relogio from './components/relogio';
 import MsgLimit from './components/msg_limit';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Aviso from './components/aviso';
 
 function App() {
   return (
@@ -14,14 +15,15 @@ function App() {
                 <h1>Exercícios Faculdade SENAI Antonio Adolpho Lobbe</h1>
                 <Link to="/relogio" className="botao">Relógio</Link>
                 <Link to="/msg_limit" className="botao">Limite de Mensagens</Link>
-              
+                <Link to="/aviso" className="botao">Aviso com temporizador</Link>
+                
               </header>
             </div>
-          }
-        />
-    <Route path="/relogio"element={<Relogio />}/>
+          }/>
 
+    <Route path="/relogio"element={<Relogio />}/>
     <Route path="/msg_limit"element={<MsgLimit />}/>
+    <Route path="/aviso"element={<Aviso />}/>
 
   </Routes>
 </BrowserRouter>
---
+++ src/components/aviso.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import "../css/aviso.css"
+
+export default function Aviso() {
+    const [tempo, setTempo] = useState(0);
+    const [bemVindo, setBemVindo] = useState("visible");
+    const [botao, setBotao] = useState("collapse");
+    
+    useEffect(()=>{
+        const intervalo = setInterval(() => {
+        setTempo((i) => i + 1); }, 1000,);
+        
+        
+        return () => clearInterval(intervalo);    
+        
+    }, []);
+    
+    useEffect(()=>{
+        if(tempo === 3){
+            Mostrar();
+        };}, [tempo]);
+    
+    
+    function Mostrar(){
+        setBemVindo("collapse");
+        setBotao("visible");
+    };
+
+    return(
+        <div className="aviso-container">
+            <p className="aviso-mensagem" style={{visibility: bemVindo}}>BEM VINDO!!!</p>
+            <p className="aviso-mensagem" style={{visibility: bemVindo}}>{tempo}</p>
+            <a className="aviso-botao" style={{visibility: botao}} href="../">Voltar</a>
+        </div>
+    );
+}
